71399bb7fd465211cc6b8886e4ae4c03be994cc0
Anthony Cabrera-Lara <anthonycabreralara@gmail.com>
2025-09-17 14:28:47 -0400

    Create Parser.h

---
+++ src/Parser.h
@@ -0,0 +1,34 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <vector>
+#include "Lexer.h" 
+
+class Node {
+public:
+    enum Type { RETURN, BINARY_OP, NUMBER, FUNCTION };
+    Type type;
+    virtual ~Node() = default;
+};
+
+class ReturnNode : public Node {
+public:
+    Node* expr;
+    ReturnNode(Node* e);
+};
+
+class NumberNode : public Node {
+public:
+    int value;
+    NumberNode(int v);
+};
+
+class FunctionNode : public Node {
+public:
+    int name;
+    TokenType returnType;
+    FunctionNode(std::string n, TokenType r);
+};
+
+Node parse(std::vector<Token>& tokens);
+#endif
